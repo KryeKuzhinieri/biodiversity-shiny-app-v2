0be4b00107b94975a7d3fe7f6850fee79fb6b050
Enes Ahmeti <sundley.12345@gmail.com>
2025-04-09 12:25:43 +0300

    added dark mode for leaflet map

--- biodiversity_shiny_app/app/view/leaflet_map.R
+++ biodiversity_shiny_app/app/view/leaflet_map.R
@@ -1,13 +1,14 @@
 box::use(
   bsicons[bs_icon],
   DBI[dbDisconnect, dbGetQuery, ],
-  bslib[as_fill_carrier, card, card_header, tooltip],
+  bslib[as_fill_carrier, card, card_header, tooltip, bs_get_variables, ],
   leaflet[
     leaflet, leafletOutput, renderLeaflet, addTiles, addAwesomeMarkers,
     awesomeIcons,
     setView, leafletProxy, clearShapes, clearMarkers, clearMarkerClusters,
+    addProviderTiles, providers,
   ],
-  shiny[showModal, span, modalDialog, moduleServer, NS, observe, req, observeEvent, onStop, div, ],
+  shiny[showModal, span, modalDialog, moduleServer, NS, observe, req, observeEvent, onStop, div, isolate, ],
   shinycssloaders[withSpinner, ],
 )
 
@@ -49,7 +50,7 @@ server <- function(id, state) {
 
     output$map <- renderLeaflet({
       leaflet() |>
-        addTiles() |>
+        addProviderTiles(providers$CartoDB.DarkMatter) |>
         setView(
           lat = 52.237049,
           lng = 21.017532,
@@ -60,13 +61,17 @@ server <- function(id, state) {
     observeEvent(input$map_marker_click,
       {
         print(input$map_marker_click)
-        # cols <- c(
-        #   "id", "scientificName", "taxonRank", "kingdom",
-        #   "continent", "country", "countryCode", "eventDate"
-        # )
-        # filtered_data <- state$data[input$map_marker_click$id, cols]
-        filtered_data <- state$data[input$map_marker_click$id, ]
-        query <- sprintf("SELECT * FROM '%s' WHERE CoreId = '%s'", table_name, filtered_data[1, "id"])
+        cols <- c(
+          "id", "scientificName", "taxonRank", "kingdom",
+          "continent", "country", "countryCode", "eventDate"
+        )
+        filtered_data <- state$data[input$map_marker_click$id, cols]
+        # filtered_data <- state$data[input$map_marker_click$id, ]
+        query <- sprintf(
+          "SELECT * FROM '%s' WHERE CoreId = '%s'",
+          table_name,
+          filtered_data[1, "id"]
+        )
         print(query)
         my_data <- dbGetQuery(conn, query)
         print(filtered_data)
@@ -88,6 +93,22 @@ server <- function(id, state) {
       },
       ignoreNULL = TRUE
     )
+
+    observe({
+      req(state$data)
+      current_theme <- bs_get_variables(
+        session$getCurrentTheme(),
+        varnames = c("theme_mode")
+      )
+      tile_provider <- if (current_theme[["theme_mode"]] == "light") {
+        providers$CartoDB.Positron
+      } else {
+        providers$CartoDB.DarkMatter
+      }
+
+      leafletProxy(ns("map")) |>
+        addProviderTiles(tile_provider)
+    })
 
     observe({
       req(state$data)
